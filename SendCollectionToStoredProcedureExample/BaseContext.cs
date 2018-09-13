e3b498fe8eefd6ac5cd883a386c66c41d6f7ebcb
matsurigoto <dog0416@gmail.com>
2018-09-14 00:54:53 +0800

    init

---
+++ SendCollectionToStoredProcedureExample/BaseContext.cs
@@ -0,0 +1,31 @@
+﻿using System.Data.Entity;
+using System.Data.SqlClient;
+using System.Configuration;
+
+
+namespace SendCollectionToStoredProcedureExample
+{
+    public class BaseContext: DbContext
+    {
+        protected string connectionName;
+
+        public BaseContext(string connName = "BaseConnection")
+            : base(connName)
+        {
+            connectionName = connName;
+        }
+
+        public BaseContext setDatabase(string databaseName)
+        {
+            var connectionString = ConfigurationManager.AppSettings[connectionName];
+
+            SqlConnectionStringBuilder builder = new SqlConnectionStringBuilder(connectionString);
+            //change the database before creating the new connection
+            builder.InitialCatalog = databaseName;
+
+            string sqlConnectionString = builder.ConnectionString;
+
+            return new BaseContext(sqlConnectionString);
+        }
+    }
+}
---
+++ SendCollectionToStoredProcedureExample/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace SendCollectionToStoredProcedureExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BaseContext db = new BaseContext("localhost");
+            var db1New = db.setDatabase("MOE");
+  
+            var sql = "usp_GameAction_{GameProviderCode}_GetUnprocessedBetData {0},{1}";
+            var result = db.Database.SqlQuery<PlayerData>(sql, SearchParameter);
+
+        }
+    }
+
+    class PlayerData
+    {
+        public Guid Id { get; set; }
+        public string name { get; set; }
+    }
+}
